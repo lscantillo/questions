9c996df06e3b20fbad233bf8de7fe286ada9cb94
d4devil <damian.moreno@wizeline.com>
2022-02-15 17:54:02 -0600

    Add: seed mapping rules

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,31 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+mapping_rules = {
+  questions: {
+    question_id: 'id',
+    question: 'text_content',
+    assigned_department: 'department_id',
+    created_by_employee_id: 'employee_id'
+  },
+  answers: {
+    answer_id: 'id',
+    answered_text: 'text_content',
+    answered_by_employee_id: 'employee_id',
+    answered_question_id: 'question_id'
+  },
+  comments: {
+    questionId: 'question_id',
+    comment: 'text_content'
+  },
+  votes: {
+    user: 'employee_id',
+    is_upvote: 'is_up_vote'
+  },
+  tags: {
+    tag_id: 'id'
+  },
+  employees: {
+    name: 'full_name'
+  }
+ }
+
+
