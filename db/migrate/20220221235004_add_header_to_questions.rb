6f77cd75736fd4c2bc6097618b2c73a180f2bcf3
d4devil <damian.moreno@wizeline.com>
2022-02-22 10:46:22 -0600

    Fixed: text_content field on questions was renamed to header, create a brand new text_content without limit

--- db/migrate/20220221235004_add_header_to_questions.rb
+++ db/migrate/20220221235004_add_header_to_questions.rb
@@ -3,15 +3,6 @@ class AddHeaderToQuestions < ActiveRecord::Migration[7.0]
     rename_column :questions, :text_content, :header
     change_column :questions, :header, :string, limit: 500
     
-    create_table :text_contents do |t|
-      t.string :text , limit: 500
-    end
-
-    create_table :questions_optional_contents
-    add_reference :questions_optional_contents, :questions, foreign_key: true
-    add_reference :questions_optional_contents, :content, foreign_key: true, polymorphic: true
-
-
-    
+    add_column :questions, :text_content, :string, default: nil
   end
 end
