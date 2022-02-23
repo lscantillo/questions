e5e35d4907e66ec3cfae1e159e31ad6894432ca2
d4devil <damian.moreno@wizeline.com>
2022-02-23 11:04:22 -0600

    Created: JWT Decoder service

---
+++ app/services/json_web_token.rb
@@ -0,0 +1,15 @@
+module JsonWebToken
+  class Encoder
+    SECRET_KEY = Rails.application.secrets.secret_key_base.to_s
+
+    def self.encode(playload, exp = 24.hours.from_now)
+      playload[:exp] = exp.to_i
+      JWT.encode(playload, SECRET_KEY)
+    end
+
+    def self.decode(token)
+      decoded = JWT.decode(token, SECRET_KEY)[0]
+      HashWithIndifferentAccess.new decoded
+    end
+  end
+end
