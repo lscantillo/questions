5a28a513e135e8f6a2cf4201cd14ce4b7e01a3d8
d4devil <damian.moreno@wizeline.com>
2022-02-23 11:04:59 -0600

    Added: Authorize request method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,20 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  def authorize_request
+    header = request.headers['Authorization']
+    header = header.split(' ').last if header
+
+    begin
+      @decoded = JsonWebToken::Encoder.decode(header)
+      @current_user = Employee.find_by_email(@decoded['email'])
+
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { error: e.message }, status: :unauthorized
+      
+    rescue JWT::DecodeError => e
+      render json: { error: e.message }, status: :unauthorized
+      
+  end
+
 end
