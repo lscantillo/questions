582c12806150b0b90f68c7af732ec84e552089a2
d4devil <damian.moreno@wizeline.com>
2022-02-23 15:14:45 -0600

    WIP

--- app/controllers/api/questions_controller.rb
+++ app/controllers/api/questions_controller.rb
@@ -1,9 +1,20 @@
 class QuestionsController < ApplicationController
   before_action :authorize_request
+  before_action :get_user, exept: %i[index create]
   def index
+    
   end
 
   def show
+    
+  end
 
+  def get_user
+    begin
+      user = User.find_by_id(params[:id])
+
+    rescue ActiveRecord::RecordNotFound => e
+      render :json {:error e.message}
+    end
   end
 end
