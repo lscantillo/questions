b2a9d6ac61d0ccecb0508909df33011bc27c8e8c
d4devil <damian.moreno@wizeline.com>
2022-02-24 16:53:43 -0600

    Added: rest_api_controller for generic api behaviour && change questions controller to mixin with it

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :questions
+  namespace :api, defaults: { format: 'json' } do
+    resources :questions
+  end
 end
