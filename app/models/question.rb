f333c7b423bc9244de55f38a7933a82a4794b11f
d4devil <damian.moreno@wizeline.com>
2022-03-01 16:20:40 -0600

    Fixed: Questions sorted, added permited params and pagination

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,11 +2,10 @@ class Question < ApplicationRecord
   belongs_to :employee, optional: true
   belongs_to :assigned_to_employee, class_name: 'Employee', optional: true
   belongs_to :department, optional: true
+  belongs_to :location, optional: true
 
   has_many :comments
   has_many :questions_tags
   has_many :tags, through: :questions_tags
-
   has_many :votes
-  belongs_to :location
 end
