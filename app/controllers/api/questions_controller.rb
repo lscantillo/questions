8de6db438742d402891acfdaea4b7045419d17e8
d4devil <damian.moreno@wizeline.com>
2022-03-01 10:50:40 -0600

    Added: Questions pagination

--- app/controllers/api/questions_controller.rb
+++ app/controllers/api/questions_controller.rb
@@ -1,6 +1,11 @@
-
 class Api::QuestionsController < ApplicationController
+  include Pagy::Backend
   include RestApiController
-
+  after_action { pagy_headers_merge(@pagy) if @pagy }
   @permited_params = %i[header text_content]
+
+  def index
+    @pagy, @questions = pagy(Question.all)
+    render json: { data: @questions, pagy: pagy_metadata(@pagy) }
+  end
 end
