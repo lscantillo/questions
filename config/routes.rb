973e96a017f92b4788be9482cde53d19b23e7db6
lscantillowl <luis.cantillo@wizeline.com>
2022-03-01 20:44:26 -0500

    Fix: Change to post method in similar questions

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
     resources :locations
     get '/tones', to: 'tones#tone'
     resources :comments
-    get 'similar_questions' => 'validations#similar_questions'
+    post 'similar_questions' => 'validations#similar_questions'
     get 'hottest' => 'validations#hottest'
     get 'insteresting' => 'validations#interesting'
   end
