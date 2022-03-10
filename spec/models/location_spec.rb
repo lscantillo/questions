5d5250c9bb4d9595e147bcdf31ca2ac223b121c8
lscantillowl <luis.cantillo@wizeline.com>
2022-03-10 17:02:43 -0500

    Rspec: test for some models

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Location, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Associations' do
+    it { should have_many(:questions) }
+  end
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Associations' do
+    it { should have_many(:questions).through(:questions_tags) }
+    it { should have_many(:questions_tags) }
+  end
 end
