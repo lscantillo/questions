b3ac96a2491b07bc357c757fc8ec7937f46b067d
aldo-leon <aldo.leon@wizeline.com>
2022-03-24 09:54:07 -0600

    wip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,4 @@
 # frozen_string_literal: true
-class Api::EmptyHeader < StandardError
-  attr_reader :code
-
-  def initialize(code)
-    super
-    @code = code
-  end
-end
 
 class ApplicationController < ActionController::API
   def authorize_request
@@ -14,18 +6,17 @@ class ApplicationController < ActionController::API
     header = header.split.last if header
 
     begin
-      raise Api::EmptyHeader.new(888) if header.nil?
+      # raise EmptyHeader.new(888) if header.nil?
       @decoded = JsonWebToken::Encoder.decode(header)
       raise JWT::ExpiredError, header if (Time.now <=> Time.at(@decoded['exp'])) == 1
 
       @current_user = Employee.find_by_email(@decoded['email'])
-    rescue Api::EmptyHeader => e
-      render json: { code: e.code, error: "No authorization token provided in header" }, status: :unauthorized
+    # rescue EmptyHeader => e
+    #   render json: { code: e.code, error: "No authorization token provided in header" }, status: :unauthorized
     rescue ActiveRecord::RecordNotFound => e
       render json: { error: e.message }, status: :not_found
     rescue JWT::DecodeError => e
-      # render json: { error: e.message }, status: :internal_error
-      render json: { error: "Could not decode authetication token", status: 401}
+      render json: { error: "Could not decode authetication token", status_code: 401}, status: :unauthorized
     rescue JWT::ExpiredError => e
       render json: { error: e.message }, status: :unauthorized
     end
---
+++ app/errors/api/empty_header.rb
@@ -0,0 +1,8 @@
+class EmptyHeader < StandardError
+  attr_reader :code
+
+  def initialize(code)
+    super
+    @code = code
+  end
+end
