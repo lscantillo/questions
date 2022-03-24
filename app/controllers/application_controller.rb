dff6caf0f1f0af7d28113ef2868d5f672072f1f5
aldo-leon <aldo.leon@wizeline.com>
2022-03-24 09:58:14 -0600

    remove puts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,6 @@ class ApplicationController < ActionController::API
         profile_picture_url: @decoded['picture']
       }
       @current_user = Employee.find_or_create_by(employee_params)
-      puts @current_user.full_name
     rescue ActiveRecord::RecordNotFound => e
       render json: { error: e.message }, status: :not_found
     rescue JWT::DecodeError => e
