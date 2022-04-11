65050b9b817b032221cbc667cc05d0fc670697ae
d4devil <damian.moreno@wizeline.com>
2022-04-11 17:39:33 -0500

    Fixed: Is anonymous validation

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -3,4 +3,8 @@ class Api::CommentsController < ApplicationController
   def permited_params
     %i[text_content is_answer session_hash employee_id question_id]
   end
+
+  def create_callback
+    @resource.employee = @current_user unless params[:is_anonymous].nill? || params[:is_anonymous]
+  end
 end
